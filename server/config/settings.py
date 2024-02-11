d6747c5050c047c2cece2f4f0017fa34d6f0c9f9
Tayu <107281831+Tayuchi@users.noreply.github.com>
2024-02-11 13:39:07 +0900

    シークレットキーのランダム化

--- server/config/settings.py
+++ server/config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#_z)eo==bb5g4ehup$r68_w2v@0um%pusrq+mcs0xu**b-(1f'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
